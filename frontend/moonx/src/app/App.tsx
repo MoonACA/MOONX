8112aa0bd062143ff6272547bb7902b8fdfc14ce
Etiosa <115146462+sir-mills@users.noreply.github.com>
2024-09-09 01:57:11 -0600

    chore: configured mande network chain

--- frontend/moonx/src/app/App.tsx
+++ frontend/moonx/src/app/App.tsx
@@ -1,15 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  Chain,
+  getDefaultConfig,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
+const mande = {
+  id: 18071918,
+  name: "Mande",
+  nativeCurrency: { name: "Mande", symbol: "MAND", decimals: 18 },
+  rpcUrls: {
+    default: { http: ["https://mande-mainnet.public.blastapi.io"] },
+  },
+} as const satisfies Chain;
+
 const config = getDefaultConfig({
   appName: "My RainbowKit App",
   projectId: "YOUR_PROJECT_ID",
-  chains: [mainnet, polygon, optimism, arbitrum, base],
+  chains: [mande],
   ssr: true,
 });
 
