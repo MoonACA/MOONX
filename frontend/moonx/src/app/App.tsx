ca9e5fdd4ee756b128e5c038840a10ec760a1ad3
Etiosa <115146462+sir-mills@users.noreply.github.com>
2024-09-11 23:10:45 -0600

    app fix

--- frontend/moonx/src/app/App.tsx
+++ frontend/moonx/src/app/App.tsx
@@ -7,7 +7,6 @@ import {
   RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
